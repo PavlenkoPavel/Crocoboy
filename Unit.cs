01f9adebb64a8faf5cea189348d601e5b2aa80db
PavlenkoPavel <64706879+PavlenkoPavel@users.noreply.github.com>
2021-04-04 10:37:15 +0300

    Add files via upload

---
+++ Unit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+
+{
+
+ public virtual void ReceiveDamage()
+    {
+
+        Die();
+    }
+
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+}
